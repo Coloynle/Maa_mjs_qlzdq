8e3be887bc1b3eed7d5ba2b25d94a171d558f480
2451123316 <2451123316@qq.com>
2026-07-02 11:44:00 +0800

    7.2凌晨的最后一次提交

--- agent/main.py
+++ agent/main.py
@@ -18,6 +18,7 @@ from maa.custom_action import CustomAction
 from maa.context import Context
 from maa.pipeline import JRecognitionType, JOCR
 import cv2
+
 from maa.tasker import Tasker
 
 # 默认武将列表（当所有来源都为空时回退）
